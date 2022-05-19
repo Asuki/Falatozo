d85b3c46e2c7684bd36d98492fa1c74d0a8ca210
ANY\seng <seng@anyrt.hu>
2022-05-19 08:30:07 +0200

    Settings elkészítése

--- app/src/main/java/hu/anyrt/falatozo/presentation/HomeFragment.kt
+++ app/src/main/java/hu/anyrt/falatozo/presentation/HomeFragment.kt
@@ -1,16 +1,17 @@
 package hu.anyrt.falatozo.presentation
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ListView
+import androidx.fragment.app.Fragment
 import hu.anyrt.falatozo.R
 
 class HomeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private lateinit var listViewDays: ListView
+    private lateinit var listViewMenu: ListView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +22,16 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        val rootView = inflater.inflate(R.layout.fragment_home, container, false)
+        (activity as? MainActivity)?.supportActionBar?.title = getString(R.string.menuOrder)
+        initViews(rootView)
+        return rootView
+    }
+
+    private fun initViews(rootView: View?) {
+        if(rootView != null) {
+            listViewDays = rootView.findViewById(R.id.listViewDays)
+            listViewMenu = rootView.findViewById(R.id.listViewMenu)
+        }
     }
 }
---
+++ app/src/main/java/hu/anyrt/falatozo/presentation/SettingsActivity.kt
@@ -0,0 +1,55 @@
+package hu.anyrt.falatozo.presentation
+
+import android.content.SharedPreferences
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.google.android.material.switchmaterial.SwitchMaterial
+import hu.anyrt.falatozo.R
+
+class SettingsActivity : AppCompatActivity() {
+
+    private lateinit var switchMenuNotification: SwitchMaterial
+    private lateinit var sharedPreferences: SharedPreferences
+    private lateinit var sharedEditor: SharedPreferences.Editor
+    private val TAG = this::class.java.simpleName
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+
+        init()
+    }
+
+    private fun init() {
+        sharedPreferences = getSharedPreferences("falatozo_shared_pref", MODE_PRIVATE)
+        sharedEditor = sharedPreferences.edit()
+        initViews()
+        initEvents()
+    }
+
+    private fun initViews() {
+        // switch kódjának összekapcsolása a nézettel
+        switchMenuNotification = findViewById(R.id.switchMenuNotificationSettings)
+        // elmentett switch pozíció beállítása
+        switchMenuNotification.isChecked =
+            sharedPreferences.getBoolean(getString(R.string.menu_notification_pref),
+                false)
+    }
+
+    private fun initEvents() {
+        // Event: Ha klikkel a felhasználó és változik a switch true->fals vagy false->true
+        switchMenuNotification.setOnCheckedChangeListener { buttonView, isChecked ->
+            // mentjük a beállítás
+            // Ezzel tesszük bele a beállítások fájlba
+            sharedEditor.putBoolean(getString(R.string.menu_notification_pref), isChecked)
+            // Az apply-al aszinkron módon mentjük. Ez kell ahhoz,
+            // hogy utána maradandó legyen a változtatás
+            sharedEditor.apply()
+            // Egyszerű üzenet a felhasználónak
+            Toast.makeText(this@SettingsActivity,
+                "Feliratkozás változtatva ${isChecked}",
+                Toast.LENGTH_SHORT).show()
+        }
+    }
+}
